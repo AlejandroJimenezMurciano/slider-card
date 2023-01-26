690e79636496885063a8de0762527f4530f04e35
AlejandroJimenezMurciano <alejimenezmurciano@gmail.com>
2023-01-26 17:14:08 +0100

    card-slider

---
+++ src/js/constants.js
@@ -0,0 +1,24 @@
+import { loadConfigFromFile } from 'vite';
+
+const INFO = {
+  card: {
+    title: 'Title 1',
+    description: 'Loremipsum'
+  },
+  card: {
+    title: 'Title 2',
+    description: 'Loremipsum'
+  },
+  card: {
+    title: 'Title 3',
+    description: 'Loremipsum'
+  },
+  card: {
+    title: 'Title 4',
+    description: 'Loremipsum'
+  },
+  card: {
+    title: 'Title 5',
+    description: 'Loremipsum'
+  }
+};
